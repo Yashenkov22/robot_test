d273b46b9f04813d81cbe3aa3666d490abad7dab
Vlad <yashenkov.v@yandex.ru>
2023-09-26 17:38:31 +0300

    task 2 done

--- R4C/robots/services.py
+++ R4C/robots/services.py
@@ -1,14 +1,26 @@
 import json
+import sqlite3
+import os
+
+import pandas
 
 from django.http import HttpRequest
 from .forms import RobotForm
+
+
+query_to_excel = '''
+SELECT model, version, count(version)
+FROM robots_robot
+WHERE model = "{}" AND created >= datetime("now", "-7 day")
+GROUP by version
+'''
 
 
 def valid_request(request: HttpRequest) -> dict | str:
     if request.content_type == 'application/json':
         data: dict = json.loads(request.body)
         form = RobotForm(data)
-        
+
         try:
             if form.is_valid():
                 data = form.cleaned_data
@@ -21,3 +33,19 @@ def valid_request(request: HttpRequest) -> dict | str:
             return 'incorrect "created" field value'
     
     return 'incorrect content-type'
+
+
+def make_excel_file(models: list):
+    conn = sqlite3.connect('db.sqlite3')
+    models = map(lambda x: x[0], models)
+
+    with pandas.ExcelWriter('output.xlsx') as writer:
+        
+        for model in models:  
+            df = pandas.read_sql(query_to_excel.format(model),
+                                 conn)
+            
+            df.to_excel(writer,
+                        sheet_name=f'model {model}',
+                        index=False,
+                        header=['Модель', 'Версия', 'Количество за неделю'])
--- R4C/robots/views.py
+++ R4C/robots/views.py
@@ -1,12 +1,18 @@
-from django.http import HttpRequest, JsonResponse
-from django.core.exceptions import ObjectDoesNotExist
+import os
+
+from django.shortcuts import render
+from django.http import HttpRequest, JsonResponse, HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.http import require_POST
 
-from .services import valid_request
+from .services import valid_request, make_excel_file
 from .models import Robot
 
 
+def index(request):
+    return render(request, 'robots/download.html')
+
+#Add new record to database
 @require_POST
 @csrf_exempt
 def new_robot_record(request: HttpRequest):
@@ -14,17 +20,24 @@ def new_robot_record(request: HttpRequest):
 
     if isinstance(data, dict):
 
-        try:
-            Robot.objects.get(serial=data['serial'])
-        except ObjectDoesNotExist:
-            Robot.objects.create(**data)
-            return JsonResponse({'status': 'success',
-                                'detail': 'object has been created'})
-        else:
-            return JsonResponse({'status': 'error',
-                                 'detail': 'object already exists'})
+        Robot.objects.create(**data)
+        return JsonResponse({'status': 'success',
+                            'detail': 'object has been created'})
         
     else:
         return JsonResponse({'status': 'error',
                             'detail': data})
 
+
+def download_excel(request):
+    models = Robot.objects.values_list('model').distinct()
+    make_excel_file(models)
+
+    with open('output.xlsx', 'rb') as f:
+        response = HttpResponse(f, content_type='application/force-download')
+        response['Content-Disposition'] = 'attachment; filename="File.xlsx"'
+
+    if os.path.exists('output.xlsx'):
+        os.remove('output.xlsx')
+    
+    return response
